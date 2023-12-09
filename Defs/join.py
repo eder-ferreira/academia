7437b8529db79499bfbcd42848553e8a1ff2825e
ederpferreira <eder.ferreira.ads@gmail.com>
2023-12-09 13:03:21 -0400

    - atualização relatorios aniversariantes do mes

--- Defs/join.py
+++ Defs/join.py
@@ -1,3 +1,4 @@
+import datetime as date
 from prettytable import PrettyTable
 import sqlite3
 database = '/Users/ederpferreira/PycharmProjects/academia/academia.db'
@@ -8,7 +9,8 @@ cur = con.cursor()
 def join_funcionario_cargo():
     # Executar a consulta INNER JOIN
     cur.execute("""
-        SELECT tb_funcionario.id, tb_funcionario.num_documento, tb_funcionario.nome, tb_funcionario.matricula, tb_cargo.nome, tb_cargo.setor, tb_cargo.salario_base
+        SELECT tb_funcionario.id, tb_funcionario.nome, tb_funcionario.matricula, 
+        tb_cargo.nome, tb_cargo.setor, tb_cargo.salario_base
         FROM tb_funcionario
         INNER JOIN tb_cargo ON tb_funcionario.cargo_id = tb_cargo.codigo
     """)
@@ -21,21 +23,18 @@ def join_funcionario_cargo():
     #     print(resultado)
 
     tabela = PrettyTable()
-    tabela.field_names =["Id","Doc_Identificação","Funcionario", "Matricula", "Cargo", "Setor", "Salario"]
+    tabela.field_names =["Id","Funcionario", "Matricula", "Cargo", "Setor", "Salario"]
     for row in resultados:
         tabela.add_row(row)
     print(tabela)
-
+join_funcionario_cargo()
 
 
 def salarios():
 
-    escolha = input("""
-    Digite o periodo Ex.:Top 5 =>  """)
+    escolha = input("""Digite a quantidade de linhas Ex.:Top 5 =>  """)
 
-    cur.execute(f""" SELECT * FROM tb_cargo ORDER BY salario_base DESC LIMIT {escolha} """)
-
-
+    cur.execute(f""" SELECT nome, setor, salario_base FROM tb_cargo ORDER BY salario_base DESC LIMIT {escolha} """)
     # Obter os resultados
     resultados = cur.fetchall()
 
@@ -49,6 +48,17 @@ def salarios():
         tabela.add_row(row)
     print(tabela)
 
+
 def aniversario():
 
-    print("Em construção!!!")
+    # cur.execute("""SELECT strftime('%d-%m-%Y', 'now')""")
+    # cur.execute("""SELECT * FROM tb_funcionario WHERE dt_nascimento BETWEEN '1900-01-01' AND '2000-01-01'""")
+    mes = input("Insira o mês dos aniversariantes => ")
+    cur.execute(f"""SELECT nome, dt_nascimento FROM tb_funcionario WHERE strftime('%m', dt_nascimento) = '{mes}'""")
+
+    resultados = cur.fetchall()
+    tabela = PrettyTable()
+    tabela.field_names = [desc[0] for desc in cur.description]
+    for row in resultados:
+        tabela.add_row(row)
+    print(tabela)
